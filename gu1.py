d8743f92e0bb210b2ae4c6a44f0f6e8485f3df49
Vislyy <maksimslepuch@gmail.com>
2025-04-27 12:09:22 +0300

    gu1
    dfghiop[ouyrdsdfguioihfsadghoihgx

---
+++ gu1.py
@@ -0,0 +1,20 @@
+from customtkinter import *
+
+window = CTk()
+window.geometry("500x500")
+window.title("lohika")
+window.configure(fg_color="blue")
+knopka = CTkButton(window, text="тапай хомячка")
+knopka.pack()
+nadpus = CTkLabel(window, text="хомяк")
+nadpus.pack()
+
+
+
+
+
+
+
+
+
+window.mainloop()
